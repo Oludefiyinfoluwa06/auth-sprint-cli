ef83a4136a5cec76acbbc74efd0dcfe9b501bfc2
Timothy <oludefiyinfoluwa06@gmail.com>
2025-11-12 00:16:16 +0100

    Configure for publish to npm

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+#!/usr/bin/env node
 import { Command } from 'commander';
 import { generateAuthBoilerplate } from './commands/create-auth.js';
 
 const program = new Command();
 
 program
-  .name('auth-boilerplate-cli')
+  .name('Auth Sprint')
   .description('CLI for managing authentication boilerplate tasks')
   .version('1.0.0')
-  .command('create-auth')
   .description('Create a new authentication boilerplate')
   .option('-f, --framework <type>', 'Specify the framework (e.g., express.js)')
   .option('-s, --strategy <type>', 'Specify the authentication strategy (e.g., JWT, OAuth)')
