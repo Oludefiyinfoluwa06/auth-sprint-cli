61334ccf906ebd8259ae87c78220e9f10d8add3c
Timothy <oludefiyinfoluwa06@gmail.com>
2025-11-11 13:05:57 +0100

    Write test cases

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,11 +16,15 @@ jest.unstable_mockModule('../prompts/select-options.prompt.js', () => ({
   requireOptions: jest.fn(),
 }));
 
+jest.unstable_mockModule('../frameworks/express.handler.js', () => ({
+  handleExpressSetup: jest.fn(),
+}));
+
 const fsUtils = await import('../utils/fs.utils.js');
 const logger = await import('../helpers/logger.helper.js');
 const prompt = await import('../prompts/select-options.prompt.js');
 const { generateAuthBoilerplate } = await import('../commands/create-auth.js');
-
+const expressHandler = await import('../frameworks/express.handler.js');
 
 describe('generateAuthBoilerplate', () => {
   test('log error if directory does not contain package.json file', async () => {
@@ -37,5 +41,19 @@ describe('generateAuthBoilerplate', () => {
     await generateAuthBoilerplate({ framework: 'express.js', strategy: 'jwt' });
 
     expect(logger.log.error).toHaveBeenCalledWith('You need to set up an express project');
+  });
+
+  test('calls handleExpressSetup when package.json exists and framework is express.js', async () => {
+    fsUtils.readDir.mockResolvedValue(['package.json']);
+    prompt.requireOptions.mockResolvedValue({
+      framework: 'express.js',
+      strategy: 'jwt',
+    });
+
+    expressHandler.handleExpressSetup.mockResolvedValueOnce();
+
+    await generateAuthBoilerplate({ framework: 'express.js', strategy: 'jwt' });
+
+    expect(expressHandler.handleExpressSetup).toHaveBeenCalledTimes(1);
   });
 });
